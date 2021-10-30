a81695b1b81e73d7fd3bd116a1d6f206b4dee31b
Tom Streller <tom@streller.me>
2021-10-30 20:17:37 +0200

    asset cleanup

--- game_plugin/src/loading.rs
+++ game_plugin/src/loading.rs
@@ -23,18 +23,38 @@ impl Plugin for LoadingPlugin {
 
 #[derive(AssetCollection)]
 pub struct FontAssets {
-    #[asset(path = "fonts/FiraSans-Bold.ttf")]
-    pub fira_sans: Handle<Font>,
+    #[asset(path = "fonts/kenney-future.ttf")]
+    pub kenney_future: Handle<Font>,
 }
 
 #[derive(AssetCollection)]
 pub struct AudioAssets {
-    #[asset(path = "audio/flying.ogg")]
-    pub flying: Handle<AudioSource>,
+    #[asset(path = "audio/coin3.ogg")]
+    pub coin3: Handle<AudioSource>,
+    #[asset(path = "audio/engine4.ogg")]
+    pub engine4: Handle<AudioSource>,
+    #[asset(path = "audio/explosion1.ogg")]
+    pub explosion1: Handle<AudioSource>,
+    #[asset(path = "audio/gameover2.ogg")]
+    pub gameover2: Handle<AudioSource>,
+    #[asset(path = "audio/hit1.ogg")]
+    pub hit1: Handle<AudioSource>,
+    #[asset(path = "audio/hit2.ogg")]
+    pub hit2: Handle<AudioSource>,
+    #[asset(path = "audio/music-comedy.ogg")]
+    pub music_comedy: Handle<AudioSource>,
+    #[asset(path = "audio/upgrade4.ogg")]
+    pub upgrade4: Handle<AudioSource>,
+    #[asset(path = "audio/upgrade5.ogg")]
+    pub upgrade5: Handle<AudioSource>,
 }
 
 #[derive(AssetCollection)]
 pub struct TextureAssets {
-    #[asset(path = "textures/bevy.png")]
-    pub texture_bevy: Handle<Texture>,
+    #[asset(texture_atlas(tile_size_x = 8.0, tile_size_y = 8.0, columns = 12, rows = 10, padding_x = 0.0, padding_y = 0.0))]
+    #[asset(path = "textures/tiles_packed.png")]
+    tiles: Handle<TextureAtlas>,
+    #[asset(texture_atlas(tile_size_x = 32.0, tile_size_y = 32.0, columns = 4, rows = 2, padding_x = 0.0, padding_y = 0.0))]
+    #[asset(path = "textures/ships_packed.png")]
+    ships: Handle<TextureAtlas>,
 }
